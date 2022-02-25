433b15fe885078732f3a12a6816bf0d7862e8eca
Babatunde Haastrup <babatunde.haastrup@vodafone.com>
2022-02-25 15:20:32 +0000

    Added Final action settings

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello again from my laptop!'),
+        body: JSON.stringify('Hello again from my GitHub account!'),
     };
     return response;
 };
